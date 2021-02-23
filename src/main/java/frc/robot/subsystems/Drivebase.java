3c006c18dce6a833185024b72f24091f5ce07d98
Amalan Iyengar <amalan.iyengar@gmail.com>
2021-02-23 15:57:58 -0500

    Added encoders to drivebase

--- src/main/java/frc/robot/subsystems/Drivebase.java
+++ src/main/java/frc/robot/subsystems/Drivebase.java
@@ -6,6 +6,9 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import com.revrobotics.CANEncoder;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
@@ -20,9 +23,15 @@ public class Drivebase extends SubsystemBase {
 
   private DifferentialDrive diffDrive = new DifferentialDrive(leftLeader, rightLeader);
 
+  private CANEncoder leftEncoder, rightEncoder;
+
   public Drivebase() {
     leftFollower.follow(leftLeader);
     rightFollower.follow(rightLeader);
+
+    leftEncoder = leftLeader.getEncoder();
+    rightEncoder = rightLeader.getEncoder();
+
   }
 
   public void arcadeDrive(double speed, double rotation) {
@@ -32,6 +41,8 @@ public class Drivebase extends SubsystemBase {
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    SmartDashboard.putNumber("Left encoder", leftEncoder.getPosition());
+    SmartDashboard.putNumber("Right encoder", rightEncoder.getPosition());
   }
 
   @Override
